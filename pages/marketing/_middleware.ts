eacda9e27e89557549c36af7d6e63415d6fdb86b
lidqqq <lidqqq.dev@gmail.com>
2022-02-20 11:50:15 +0900

    fix

--- pages/marketing/_middleware.ts
+++ pages/marketing/_middleware.ts
@@ -1,18 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { getBucket } from '@lib/ab-testing'
-import { MARKETING_BUCKETS } from '@lib/buckets'
+import { getBucket } from "@lib/ab-testing";
+import { MARKETING_BUCKETS } from "@lib/buckets";
+import { NextRequest, NextResponse } from "next/server";
 
-const COOKIE_NAME = 'bucket-marketing'
+const COOKIE_NAME = "bucket-marketing";
 
 export function middleware(req: NextRequest) {
   // Get the bucket cookie
-  const bucket = req.cookies[COOKIE_NAME] || getBucket(MARKETING_BUCKETS)
-  const res = NextResponse.rewrite(`/marketing/${bucket}`)
+  const bucket = req.cookies[COOKIE_NAME] || getBucket(MARKETING_BUCKETS);
+  const destination = req.nextUrl.clone();
+  destination.pathname = `/marketing/${bucket}`; // or /marketing
+  const res = NextResponse.rewrite(destination);
 
   // Add the bucket to cookies if it's not there
   if (!req.cookies[COOKIE_NAME]) {
-    res.cookie(COOKIE_NAME, bucket)
+    res.cookie(COOKIE_NAME, bucket);
   }
 
-  return res
+  return res;
 }
